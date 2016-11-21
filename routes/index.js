688f093fdc7ac436620a87e2cf50384ef568539a
Fu <youtianzhan@126.com>
2016-11-21 17:20:15 +0800

    add disable cluster

--- routes/index.js
+++ routes/index.js
@@ -22,7 +22,7 @@ router.get('/', function (req, res, next) {
         cluster.name = key;
         cluster.kafkaVersion = AllCluster[key]["kafkaVersion"];
         cluster.zkHosts = AllCluster[key]["zkHosts"];
-        cluster.operation = "Disable";
+        cluster.operation = AllCluster[key]["operation"];
         clusters.push(cluster);// console.log(cluster.name,cluster.kafkaVersion,cluster.zkHosts,cluster.operation);
     }
     res.render('clusters', {clusters: clusters});
@@ -38,42 +38,80 @@ router.get('/updateCluster', function (req, res, next) {
     console.log(req.query.c);//获取要修改的cluster name
     var modifyname = req.query.c;
     var originInfo = AllCluster[modifyname];
-    res.render('updateCluster',{modifyname:modifyname,originInfo:originInfo});
+    res.render('updateCluster', {modifyname: modifyname, originInfo: originInfo});
 });
 
 
 router.post('/clusters/:clustername', function (req, res, next) {
-    var attresult = [];
+    console.log("------------------------------判断是modify 还是 disable:::" + req.body.operation);
+    var clustername = req.body.name;
+    var checkedkey = ["logkafkaEnabled", "pollConsumers", "filterConsumers", "activeOffsetCacheEnabled", "displaySizeEnabled"];
     var attsjson = req.body;
-    console.log(attsjson);
+    var changedInfo = AllCluster[clustername];//console.log(changedInfo);
+
+    if (req.body.operation == "Disable") {
+        console.log("Disable 提交了表单");
+        changedInfo["operation"] = "Disable"
+
+        res.render('changeclusterresult', {title: "Disable Cluster", clustername: clustername})
+
+    }
+    if (req.body.operation == "Update") {
+        console.log("Update 提交了表单");
+
+        changedInfo["operation"] = "Update";
+        changedInfo["zkHosts"] = attsjson.zkHosts;
+        var attsstr = JSON.stringify(attsjson);//console.log("转成字符串" + attsstr)
+        var arrs = attsstr.slice(1, -1).split(",");//console.log("分割成数组" + arrs)
+
+        for (var j = 0; j < checkedkey.length; j++) {
+            // console.log("删除已经加入的checked");
+            changedInfo["check_" + checkedkey[j]] = "";
+        }
+
+        for (var i = 3; i < arrs.length; i++) { //要从zkHost后一位开始以：分割，此时zhHosts的位置是2,
+            var temp = arrs[i].split(":");
+            var key = temp[0].slice(1, -1);
+            var value = temp[1].slice(1, -1);
+            changedInfo[key] = value;
+            if (checkedkey.indexOf(key) > -1) {
+                console.log("有checked 选项" + key)
+                changedInfo["check_" + key] = "checked";
+            }
+        }
+        res.render('changeclusterresult', {title: "Update Cluster", clustername: clustername})
+    }
+
 })
+
 
 /* 获取表单提交的数据处理后存入attresult 数组，详细见README/1.*/
 router.get('/clusters', function (req, res, next) {
-    var checkedkey = ["logkafkaEnabled","pollConsumers","filterConsumers","activeOffsetCacheEnabled","displaySizeEnabled"];
+    var checkedkey = ["logkafkaEnabled", "pollConsumers", "filterConsumers", "activeOffsetCacheEnabled", "displaySizeEnabled"];
     var attresult = [];
     var attsjson = req.query;// console.log(attsjson);
     var clustername = req.query.name
     attresult["clustername"] = clustername;
-    attresult["zkHosts"] = req.query.zkHosts; //因为zkHost里面有：，会影响下面继续以：为分隔符进行分割，先保存
+    attresult["zkHosts"] = attsjson.zkHosts; //因为zkHost里面有：，会影响下面继续以：为分隔符进行分割，先保存
     var attsstr = JSON.stringify(attsjson);//console.log(attsstr)
     var arrs = attsstr.slice(1, -1).split(",");
     for (var i = 2; i < arrs.length; i++) {
         var temp = arrs[i].split(":");
         attresult[temp[0].slice(1, -1)] = temp[1].slice(1, -1)
     }
-    for (var j = 0; j < checkedkey.length;j++){
+    for (var j = 0; j < checkedkey.length; j++) {
         !function (j) {
-            if (attresult.hasOwnProperty(checkedkey[j])){
-                attresult["check_"+ checkedkey[j]] = "checked";
+            if (attresult.hasOwnProperty(checkedkey[j])) {
+                attresult["check_" + checkedkey[j]] = "checked";
             } else {
                 attresult["check_" + checkedkey[j]] = "";
             }
         }(j)
     }
+    console.log(attresult);
     AllCluster[attresult["clustername"]] = attresult;
     AllCluster.length++;
-    res.render('addclusterresult', {clustername: clustername})
+    res.render('changeclusterresult', {title: "Add Cluster", clustername: clustername})
 });
 
 /*每个cluster详情页*/
@@ -183,7 +221,7 @@ router.get('/clusters/test/createResult', function (req, res, next) {
     var kafka2 = require('kafka-node'),
         Producer = kafka2.Producer,
         client = new kafka2.Client(kafka2connstr);
-        producer = new Producer(client)
+    producer = new Producer(client)
     producer.on('ready', function () {
         producer.createTopics(topic_name, false, function (err, data) {
             if (err) {
